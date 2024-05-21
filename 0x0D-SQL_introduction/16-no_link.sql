5b013bfedc5fabd17fc0432ce89947f8ca9a58b9
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-05-21 14:31:32 -0700

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 --  a script that lists all records of the table second_table
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+HAVING name IS NOT NULL
+ORDER BY score DESC;
