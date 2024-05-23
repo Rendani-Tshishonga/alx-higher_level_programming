05f1ac4e5c1227920a0935d518a1ae471170cf6e
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-05-23 16:06:47 -0700

    genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -6,4 +6,4 @@ FROM tv_show_genres
 RIGHT JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
