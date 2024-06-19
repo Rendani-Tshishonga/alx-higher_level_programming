1b9edc51d64b19006574dc7b00b1862244c4cc5e
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-06-19 07:39:39 -0700

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arg = process.argv;
+
+if (arg.length <= 2){
+  console.log('No Arguments');
+} else if (arg.length === 3){
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
