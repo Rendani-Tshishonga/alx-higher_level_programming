6a53d3cb8174e39600d34d9d7bea55f324342136
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-07-09 11:21:55 -0700

    What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Write a script that fetches https://alx-intranent.hbtn.io/status
+"""
+import requests
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print("Body response:")
+    print("\t- type: {}".format(type(t)))
+    print("\t-content: {}".format(t))
