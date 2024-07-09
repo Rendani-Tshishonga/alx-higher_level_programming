a564bea417e979d98e1330ef644adabccf52d666
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-07-09 09:39:10 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,7 +11,13 @@ if __name__ == "__main__":
     We will use the with statement to return the response of the 
     HTTP server.
     """
-    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        the_page = response.read()
-        print(the_page)
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with request.urlopen(url) as response:
+        r = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode('utf-8')))
+        
     
