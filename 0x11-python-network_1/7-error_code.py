f842ccbb7a35aeff6ed0c95a03140a3d26136bdc
Rendani-Tshishonga <rendanijunior20@gmail.com>
2024-07-09 12:54:22 -0700

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,9 +8,13 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv
+    url = sys.argv[1]
+    """
+    We will get the request and return the error code if the status code is
+    greater than 400
+    """
     r = requests.get(url)
     if r.status_codes >= 400:
-        print("Error code: {}".format(r.status_code)
+        print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
